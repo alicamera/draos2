75ccbd50d68d353cab6454ed2be5283b32e250ff
alicamera <aalic6@etf.unsa.ba>
2020-09-09 22:07:46 +0200

    ispravka

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const app =express();
 const path =  require('path');
@@ -11,3 +12,19 @@ app.get('/*', function(req, res) {
 })
 
 console.log('Console listening!');
+*/
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/KupovinaAvioKarte'));
+
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/KupovinaAvioKarte/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
